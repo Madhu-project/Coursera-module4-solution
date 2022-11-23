9d44259158b16863f52604e472d628ebd956d722
Madhu-project <112104810+Madhu-project@users.noreply.github.com>
2022-11-23 10:45:46 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var names = ["Yaakov", "John", "Jen", "Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
-
-for (var i in names) {
-  if (names[i].charAt(0) === "j" || names[i].charAt(0) === "J") {
-    runscript1(names[i]);
-  } else {
-    runscript2(names[i]);
-  }
-}
