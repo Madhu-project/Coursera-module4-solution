72d8defd98b6ff31944b3b3dc060300f915d025b
Madhu-project <112104810+Madhu-project@users.noreply.github.com>
2022-11-23 10:39:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var names = ["Madhu", "Sundar", "jayesh", "Kalpana"];
+var names = ["Yaakov", "John", "Jen", "Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
 
 for (var i in names) {
   if (names[i].charAt(0) === "j" || names[i].charAt(0) === "J") {
